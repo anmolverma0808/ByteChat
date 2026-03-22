d0a9f9221394176e71f362f048aafd0d060023aa
anmolverma0808 <anmolkverma.0808@gmail.com>
2026-03-22 19:48:40 +0530

    Changes for prod deployment

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://bytechat-api.onrender.com/api', // Example, the user will replace this
+  wsUrl: 'https://bytechat-api.onrender.com/ws-chat'
+};
